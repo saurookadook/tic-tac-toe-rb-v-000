da1e7f9a77d3946a1f0b606856505d0e7a4bdb25
Andy Maskiell <maskiella@gmail.com>
2017-11-08 00:42:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,9 +19,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# def input_to_index(user_input)
-#   "#{user_input}".to_i - 1
-# end
+def input_to_index(user_input)
+  "#{user_input}".to_i - 1
+end
 
 def move(board, index, char)
   board[index] = char
